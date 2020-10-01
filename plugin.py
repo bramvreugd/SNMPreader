31f00956ba72ac84d28729123e7056ac0325363a
bramvreugd <bram@vreugdenhil.net>
2020-10-02 00:16:43 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,4 +1,3 @@
-
 #
 #   SNMPreader Plugin
 #
@@ -15,19 +14,19 @@
 #
 ##
 """
-<plugin key="SNMPreader" name="SNMP Value Reader" author="ycahome" version="1.1.0" wikilink="m" externallink="https://www.domoticz.com/forum/viewtopic.php?f=65">
+<plugin key="SNMPreader2" name="SNMP Reader2" author="ycahome" version="1.1.1" wikilink="m" externallink="https://www.domoticz.com/forum/viewtopic.php?f=65">
+    <description>
+    * Use PRefix OID will be before all OID's<br/>
+    * In the OIDlist use the form   OID1;Typename1|OID2;Typename2...<br/>
+    * Typename can be a Domoticz typename of "Speed"<br/>
+    * When using speed the speed is calculated by the difference un Octets per interval. Speed is in Mbps. 
+    </description>
     <params>
         <param field="Address" label="Server IP" width="200px" required="true" default="192.168.1.1"/>
-        <param field="Mode1" label="OID" width="200px" required="true" default="1.3.6.1.2.1.25.3.2.1.1.2"/>
+        <param field="Mode1" label="OID prefix" width="200px" required="true" default=".1.3.6.1.2.1.2.2.1."/>
         <param field="Mode2" label="Community" width="200px" required="true" default="public"/>
         <param field="Mode4" label="Check Interval(seconds)" width="75px" required="true" default="60"/>
-        <param field="Mode3" label="Domoticz TypeName" width="200px">
-            <options>
-                <option label="Custom" value="Custom"/>
-                <option label="Text" value="Text"/>
-                <option label="Temperature" value="Temperature"  default="true" />
-            </options>
-        </param>
+        <param field="Mode5" label="OID list" width="200px" required="true" default="10.5;Speed|16.5;Speed|10.1;Speed|16.1;Speed|10.4;Speed|16.4;Speed"/>
         <param field="Mode6" label="Debug" width="75px">
             <options>
                 <option label="True" value="Debug"/>
@@ -57,43 +56,71 @@ from datetime import datetime, timedelta
 #############################################################################
 
 def onStart():
-
     global gdeviceSuffix
     global gdeviceTypeName
-
+    global glastSNMPValue
+    global interval
+    
     gdeviceSuffix = "(SNMP)"
     interval = int(Parameters["Mode4"])
 
     createDevices()
+    glastSNMPValue = list(range(1, len(Devices)+1))
+    for i in range(0,len(Devices)):
+       glastSNMPValue[i]=None
+
     if Parameters["Mode6"] == "Debug":
         DumpConfigToDebug()
     
-
     ServerIP = str(Parameters["Address"])
     snmpOID = str(Parameters["Mode1"])
     snmpCommunity = Parameters["Mode2"]
-
-    snmpDataValue = str(getSNMPvalue(ServerIP,snmpOID,snmpCommunity))
-
+    try:
+       snmpDataValue = str(getSNMPvalue(ServerIP,snmpOID,snmpCommunity))
+    except:
+      snmpDataValue= None
 
     Domoticz.Heartbeat(interval)
+    Domoticz.Log("started with "+str(len(Devices))+ " devices")
+        
     return True
 
 def onHeartbeat():
-
     ServerIP = str(Parameters["Address"])
-    snmpOID = str(Parameters["Mode1"])
+    snmpOIDPrefix = str(Parameters["Mode1"])
     snmpCommunity = Parameters["Mode2"]
-
-    # Get new information and update the devices
-    snmpDataValue = str(getSNMPvalue(ServerIP,snmpOID,snmpCommunity))
-
-    UpdateDevice(1,0,snmpDataValue)
-    Domoticz.Log("SNMP Value (" + ServerIP + "/" + snmpCommunity + "/"+ snmpOID + ") retrieved:"+snmpDataValue)
-
+    for item in Parameters["Mode5"].split('|'):
+        unitNum=Parameters["Mode5"].split('|').index(item)
+        Domoticz.Log("OID Item"+snmpOIDPrefix+item+" Unit:"+str(unitNum+1))
+            
+        params=item.split(';')
+        GetSNMPDevice(unitNum,ServerIP,snmpCommunity,snmpOIDPrefix+params[0],params[1])
 
     return True
 
+def GetSNMPDevice(Unit,ServerIP,snmpCommunity,snmpOID,TypeName):
+    global glastSNMPValue
+    # Get new information and update the devices
+    try:
+        snmpDataValue = str(getSNMPvalue(ServerIP,snmpOID,snmpCommunity))
+    except Exception as err:
+       snmpDataValue = none
+    
+    if(TypeName=="Speed" and snmpDataValue!=None):
+        if(glastSNMPValue[Unit]!=None):
+            speed=int(snmpDataValue)-glastSNMPValue[Unit]
+            if(speed<0):
+                speed=speed+0x100000000
+            speed=round(8*speed/interval/(1024*1024),2)
+            UpdateDevice(Unit+1,0,str(speed)+" MB/s")
+            Domoticz.Log("SNMP Value (" + ServerIP + "/" + snmpCommunity + "/"+ snmpOID + ") "+TypeName+" retrieved:"+snmpDataValue + " prev:" + str(glastSNMPValue[Unit]) + " speed"+ str(speed) + ")" )
+            
+        glastSNMPValue[Unit]=int(snmpDataValue)
+    else:
+        UpdateDevice(Unit+1,0,snmpDataValue)
+        Domoticz.Log("SNMP Value (" + ServerIP + "/" + snmpCommunity + "/"+ snmpOID + ") "+TypeName+" retrieved:"+snmpDataValue)
+
+    
 #############################################################################
 #                         Domoticz helper functions                         #
 #############################################################################
@@ -112,7 +139,6 @@ def DumpConfigToDebug():
 
 
 def UpdateDevice(Unit, nValue, sValue):
-
     # Make sure that the Domoticz device still exists before updating it.
     # It can be deleted or never created!
 
@@ -125,24 +151,26 @@ def UpdateDevice(Unit, nValue, sValue):
 #############################################################################
 
 def createDevices():
-
     # Are there any devices?
-    if len(Devices) != 0:
+    if len(Devices) == len(Parameters["Mode5"].split('|')):
         # Could be the user deleted some devices, so do nothing
-        Domoticz.Debug("Devices Already Exist.")
-        return
+        Domoticz.Debug("Devices already exist."+ str(len(Devices)))
+    else:
+        # Give the devices a unique unit number. This makes updating them more easy.
+        # UpdateDevice() checks if the device exists before trying to update it.
 
-    # Give the devices a unique unit number. This makes updating them more easy.
-    # UpdateDevice() checks if the device exists before trying to update it.
-
-    # Add Power and temperature device(s)
-    Domoticz.Device(Name=gdeviceSuffix, Unit=1, TypeName=Parameters["Mode3"]).Create()
-
-    Domoticz.Log("Devices created.")
-
-
+        for item in Parameters["Mode5"].split('|'):
+            unitNum=Parameters["Mode5"].split('|').index(item)+1
+        
+            if( unitNum not in Devices):
+                params=item.split(';')
+                if(params[1]!="Speed"):
+                    Domoticz.Device(Name=gdeviceSuffix, Unit=unitNum, TypeName=Params[1]).Create()
+                else:
+                    Domoticz.Device(Name=gdeviceSuffix, Unit=unitNum, TypeName="Custom", Options={"Custom": "1;Mbps"}).Create()
+        Domoticz.Log("Devices created."+ str(len(Devices)))
+    
 def getSNMPvalue(ServerIP,snmpOID,snmpCommunity):
-
     cmdGen = cmdgen.CommandGenerator()
 
     #genData = cmdgen.CommunityData('public')
@@ -172,7 +200,6 @@ def getSNMPvalue(ServerIP,snmpOID,snmpCommunity):
 #
 
 def parseIntValue(s):
-
         try:
             return int(s)
         except:
@@ -183,11 +210,7 @@ def parseIntValue(s):
 #
 
 def parseFloatValue(s):
-
         try:
             return float(s)
         except:
             return None
-
-
-
